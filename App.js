091613349594965596132e7e83e21b99d6901eaf
Naman Agarwal <namanddev7@gmail.com>
2022-01-01 23:24:11 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Demo from "./src/demo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Demo />
   );
 }
 
